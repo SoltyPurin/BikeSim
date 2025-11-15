fae761013fea17f78e5df9d2a4e9fc5c79fd1c16
SoltyPurin <Shouta1109s@gmail.com>
2025-11-15 21:22:56 +0900

    ver8.8
    ステージ中にライトを設置した

--- Assets/Scripts/TimeZone/FirstTimeZoneSet.cs
+++ Assets/Scripts/TimeZone/FirstTimeZoneSet.cs
@@ -6,8 +6,27 @@ public class FirstTimeZoneSet : MonoBehaviour
 {
     [SerializeField,Header("各時間帯ごとのDirectionalLightの回転")]
     private List<Vector3>_timeZoneDirectionalLightRotation = new List<Vector3>();
+    [SerializeField, Header("ステージのDirectionalLight")]
+    private GameObject _directionalLight = default;
     private void Awake()
     {
-        
+        int timeZoneint = PlayerPrefs.GetInt("TimeZone", 0);
+        switch (timeZoneint)
+        {
+            case 0:
+                Debug.Log("昼");
+                break;
+
+            case 1:
+                Debug.Log("夕方");
+                break;
+
+            case 2:
+                Debug.Log("夜");
+                break;
+        }
+
+        Vector3 rota = _timeZoneDirectionalLightRotation[timeZoneint];
+        _directionalLight.transform.rotation = Quaternion.Euler(rota);
     }
 }
